3a39639396d3189ccdc14614585bda66db4567bb
sanotyan1202 <taikisano1202@gmail.com>
2020-06-29 18:47:38 +0900

    使い方の区切りをhrからmarginに変更

--- src/components/HowToUse.vue
+++ src/components/HowToUse.vue
@@ -7,21 +7,18 @@
           <div class="row">
             <div class="twelve columns">
               <p>Slide Actは3ステップで簡単に使用することができます。</p>
-              <div id="step1">
-                <hr>
+              <div id="step1" class="how-to-step">
                 <h5>Step1. プレゼン資料をPDFに変換</h5>
                 <p>スライドをPDFに変換してください。</p>
                 <p>PowerPoint、Googleスライド、Beautiful.aiなどに対応しています。</p>
               </div>
-              <div id="step2">
-                <hr>
+              <div id="step2" class="how-to-step">
                 <h5>Step2. Slide Actにアップロード</h5>
                 <p>次にPDFをアップロードします。</p>
                 <p>枠内にドラッグ&ドロップをするか、ボタンを押してファイルを選択してください。</p>
                 <p><img src="img/slideact.png" class="how-to-use-image"></p>
               </div>
-              <div id="step3">
-                <hr>
+              <div id="step3" class="how-to-step">
                 <h5>Step3. 閲覧用URLを聴講者に共有</h5>
                 <p>PDFをアップロードすると、閲覧用URLが表示されます。</p>
                 <p><img src="img/browserurl.png" class="how-to-use-image"></p>
@@ -30,8 +27,9 @@
                 <p>名前とメッセージを入力し、Enterキーで送信することができます。</p>
                 <p>メッセージはプレゼンの邪魔をしないように4秒ほどで消えます。</p>
               </div>
-                <hr>
+              <div id="step4" class="how-to-step">
                 <p>いつもより活性化したプレゼンをお楽しみください。</p>
+              </div>
             </div>
           </div>
         </div>
@@ -48,5 +46,9 @@
 .how-to-use-image {
   width: 65%;
   box-shadow: 2px 2px 2px 2px gray;
+}
+
+.how-to-step {
+  margin-top:100px;
 }
 </style>
